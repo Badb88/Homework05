7d2f71065af826087aee61f625a066abcbfb4c27
OlegB <os.babkin@gmail.com>
2022-06-10 00:19:02 +0300

    Добавил решение на поиск мин и макс элементов и разницу между ними

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -7,27 +7,28 @@ double[] array = new double[6];
 
 void FillArray(double[] array)
 {
+    var RandomNumbers = new Random();
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = new Random().Next(0, 100);
+        array[i] = Math.Round(RandomNumbers.NextDouble() * 100, 2);
     }
 }
 
 void PrintArray(double[] col)
 {
-    Console.Write("Случайный массив: [");
+    Console.Write("Случайный массив: [ ");
     for (int pos = 0; pos < col.Length; pos++)
     {
         if (pos < col.Length - 1)
         {
-            Console.Write($"{col[pos]}, ");
+            Console.Write($"{col[pos]} / ");
         }
         else
         {
             Console.Write(col[pos]);
         }
     }
-    Console.Write("]");
+    Console.Write(" ]");
 }
 
 double MaxNumber(double[] array)
@@ -59,7 +60,9 @@ double MinNumber(double[] array)
 FillArray(array);
 PrintArray(array);
 Console.WriteLine();
+
 double max = MaxNumber(array);
 double min = MinNumber(array);
 double dif = max - min;
-Console.WriteLine($"Максимальный элемент массива: [{max}]; минимальный [{min}]. Разница между ними: {dif}.");
+
+Console.WriteLine($"Максимальный элемент массива: {max}; минимальный {min}. Разница между ними: {dif}.");
